29b40fab31b9c0c25573a03407c3112b8aadd1b5
faezehsajadi <a2r.sahar@gmail.com>
2025-03-04 21:01:05 +0300

    delete todocode

--- simplecode/todo.py
+++ simplecode/todo.py
@@ -20,18 +20,3 @@ def todo_list():
             break
 
 todo_list()
-import requests
-
-tasks = []
-
-def send_to_telegram(task_list):
-    token = "7885084705:AAFTh-xRG4A4P4Z0twIv8U2KfVu37HFDbvc"  # توکنی که از BotFather گرفتید
-    chat_id = "@Faezehsajadi17"  # آیدی چت یا کانال خود را وارد کنید
-    message = "Your Tasks:\n" + "\n".join([f"{i+1}. {task}" for i, task in enumerate(task_list)])
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    data = {"chat_id": chat_id, "text": message}
-    response = requests.post(url, data=data)
-    if response.status_code == 200:
-        print("Message sent successfully!")
-    else:
-        print(f"Failed to send message: {response.status_code} - {response.text}")
